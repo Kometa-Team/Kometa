181b07d3616328844e2fa32af766a662d798e921
meisnate12 <meisnate12@gmail.com>
2021-03-20 15:40:29 -0400

    fix for #133

--- plex_meta_manager.py
+++ plex_meta_manager.py
@@ -87,7 +87,7 @@ logger.info(util.get_centered_text("| |_) | |/ _ \\ \\/ / | |\\/| |/ _ \\ __/ _`
 logger.info(util.get_centered_text("|  __/| |  __/>  <  | |  | |  __/ || (_| | | |  | | (_| | | | | (_| | (_| |  __/ |   "))
 logger.info(util.get_centered_text("|_|   |_|\\___/_/\\_\\ |_|  |_|\\___|\\__\\__,_| |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   "))
 logger.info(util.get_centered_text("                                                                     |___/           "))
-logger.info(util.get_centered_text("    Version: 1.5.1                                                                   "))
+logger.info(util.get_centered_text("    Version: 1.5.1-Beta.1                                                            "))
 util.separator()
 
 if my_tests:
