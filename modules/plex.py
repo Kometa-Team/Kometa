842eff25e5e8934ed0062b4fe3c4eb83b0892e23
meisnate12 <meisnate12@gmail.com>
2021-07-12 16:21:07 -0400

    more debug

--- modules/plex.py
+++ modules/plex.py
@@ -771,11 +771,11 @@ class Plex:
     def update_item_from_assets(self, item, overlay=None):
         logger.debug(item.locations)
         logger.debug(item.locations[0])
-        self.reload(item)
-        logger.debug(item.locations)
-        logger.debug(item.locations[0])
+        logger.debug(os.path.dirname(item.locations[0]))
+        logger.debug(os.path.basename(os.path.dirname(item.locations[0])))
 
         name = os.path.basename(os.path.dirname(item.locations[0]) if self.is_movie else item.locations[0])
+        logger.debug(name)
         found_folder = False
         uploaded = False
         for ad in self.asset_directory:
