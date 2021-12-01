bdba74dcaf1234fc2ad65dcb3b694472b94af124
meisnate12 <meisnate12@gmail.com>
2021-12-01 10:17:19 -0500

    fixed anilist default year

--- modules/convert.py
+++ modules/convert.py
@@ -304,8 +304,8 @@ class Convert:
                 logger.debug(f"TMDb: {tmdb_id}, IMDb: {imdb_id}, TVDb: {tvdb_id}")
                 raise Failed(f"No ID to convert")
         except Failed as e:
-            logger.info(util.adjust_space(f"Mapping Error | {item.guid:<46} | {e} for {item.title}"))
+            logger.info(util.adjust_space(f'Mapping Error | {item.guid:<46} | {e} for "{item.title}"'))
         except BadRequest:
             util.print_stacktrace()
-            logger.info(util.adjust_space(f"Mapping Error | {item.guid:<46} | Bad Request for {item.title}"))
+            logger.info(util.adjust_space(f'Mapping Error | {item.guid:<46} | Bad Request for "{item.title}"'))
         return None, None, None
