40c92ecd7cc2fd7c451b2d712053d7cd9ede0266
meisnate12 <meisnate12@gmail.com>
2022-01-24 10:13:08 -0500

    #647 genre_mapper works again

--- modules/library.py
+++ modules/library.py
@@ -86,12 +86,12 @@ class Library(ABC):
         self.stats = {"created": 0, "modified": 0, "deleted": 0, "added": 0, "unchanged": 0, "removed": 0, "radarr": 0, "sonarr": 0}
         self.status = {}
 
-        self.tmdb_library_operation = self.assets_for_all or self.mass_genre_update or self.mass_audience_rating_update \
-                                      or self.mass_critic_rating_update or self.mass_trakt_rating_update \
+        self.items_library_operation = self.assets_for_all or self.mass_genre_update or self.mass_audience_rating_update \
+                                      or self.mass_critic_rating_update or self.mass_trakt_rating_update or self.genre_mapper \
                                       or self.tmdb_collections or self.radarr_add_all_existing or self.sonarr_add_all_existing
-        self.library_operation = self.tmdb_library_operation or self.delete_unmanaged_collections or self.delete_collections_with_less \
+        self.library_operation = self.items_library_operation or self.delete_unmanaged_collections or self.delete_collections_with_less \
                                  or self.radarr_remove_by_tag or self.sonarr_remove_by_tag or self.mass_collection_mode \
-                                 or self.genre_collections or self.genre_mapper or self.show_unmanaged
+                                 or self.genre_collections or self.show_unmanaged
         metadata = []
         for file_type, metadata_file in self.metadata_path:
             if file_type == "Folder":
