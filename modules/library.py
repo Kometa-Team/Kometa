c353f9975aad0e8f2c8e2724fd0590f9ae6d8224
meisnate12 <meisnate12@gmail.com>
2022-02-04 17:00:38 -0500

    add update_blank_track_titles

--- modules/library.py
+++ modules/library.py
@@ -75,6 +75,7 @@ class Library(ABC):
         self.radarr_remove_by_tag = params["radarr_remove_by_tag"]
         self.sonarr_add_all_existing = params["sonarr_add_all_existing"]
         self.sonarr_remove_by_tag = params["sonarr_remove_by_tag"]
+        self.update_blank_track_titles = params["update_blank_track_titles"]
         self.mass_collection_mode = params["mass_collection_mode"]
         self.metadata_backup = params["metadata_backup"]
         self.tmdb_collections = params["tmdb_collections"]
@@ -94,7 +95,7 @@ class Library(ABC):
                                        or self.tmdb_collections or self.radarr_add_all_existing or self.sonarr_add_all_existing
         self.library_operation = self.items_library_operation or self.delete_unmanaged_collections or self.delete_collections_with_less \
                                  or self.radarr_remove_by_tag or self.sonarr_remove_by_tag or self.mass_collection_mode \
-                                 or self.genre_collections or self.show_unmanaged or self.metadata_backup
+                                 or self.genre_collections or self.show_unmanaged or self.metadata_backup or self.update_blank_track_titles
 
         if self.asset_directory:
             logger.info("")
