6c44ea858b7c15b88b667d7724cfbcb9b00e0ad8
meisnate12 <meisnate12@gmail.com>
2022-05-07 17:20:22 -0400

    [74] playlist fix

--- modules/plex.py
+++ modules/plex.py
@@ -516,9 +516,7 @@ class Plex(Library):
 
     @retry(stop_max_attempt_number=6, wait_fixed=10000, retry_on_exception=util.retry_if_not_plex)
     def collection_mode_query(self, collection, data):
-        if int(collection.collectionMode) not in collection_mode_keys or collection_mode_keys[int(collection.collectionMode)] != data:
-            collection.modeUpdate(mode=data)
-            logger.info(f"Collection Mode | {data}")
+        collection.modeUpdate(mode=data)
 
     @retry(stop_max_attempt_number=6, wait_fixed=10000, retry_on_exception=util.retry_if_not_plex)
     def collection_order_query(self, collection, data):
