70b25dd88a8d728263c0c0e72541398b9eeebc4e
meisnate12 <meisnate12@gmail.com>
2022-06-23 11:31:54 -0400

    [55] fix multiple overlay names

--- modules/library.py
+++ modules/library.py
@@ -145,7 +145,6 @@ class Library(ABC):
             for file_type, overlay_file, temp_vars, asset_directory in self.overlay_path:
                 try:
                     overlay_obj = OverlayFile(self.config, self, file_type, overlay_file, temp_vars, asset_directory)
-                    self.overlay_names.extend([p for p in overlay_obj.overlays])
                     self.overlay_files.append(overlay_obj)
                     self.queue_names.extend([q for q in overlay_obj.queues])
                 except Failed as e:
