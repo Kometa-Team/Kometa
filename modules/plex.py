f5adaa77064ffd550a0428c5bd7c6a7219047fc9
meisnate12 <meisnate12@gmail.com>
2023-02-24 18:54:32 -0500

    [100] fix blank images

--- modules/plex.py
+++ modules/plex.py
@@ -1172,13 +1172,13 @@ class Plex(Library):
         if title is None:
             title = item.title
         posters, backgrounds = util.get_image_dicts(group, alias)
-        if image_set and "poster" in image_set:
+        if image_set and "poster" in image_set and image_set["poster"]:
             posters["image_set"] = image_set["poster"]
-        elif image_set and "poster_tpdb" in image_set:
+        elif image_set and "poster_tpdb" in image_set and image_set["poster_tpdb"]:
             posters["image_set"] = f"https://theposterdb.com/api/assets/{image_set['poster_tpdb']}"
-        if image_set and "background" in image_set:
+        if image_set and "background" in image_set and image_set["background"]:
             backgrounds["image_set"] = image_set["background"]
-        elif image_set and "background_tpdb" in image_set:
+        elif image_set and "background_tpdb" in image_set and image_set["background_tpdb"]:
             posters["image_set"] = f"https://theposterdb.com/api/assets/{image_set['background_tpdb']}"
         try:
             asset_poster, asset_background, item_dir, folder_name = self.find_item_assets(item, item_asset_directory=asset_location, asset_directory=asset_directory)
