4a446fd7305663287aa2c2f22795ea9546595252
meisnate12 <meisnate12@gmail.com>
2023-10-19 10:01:20 -0400

    [17] update flixpatrol with history

--- modules/flixpatrol.py
+++ modules/flixpatrol.py
@@ -3,57 +3,57 @@ from modules.util import Failed
 
 logger = util.logger
 
-ids_url = "https://raw.githubusercontent.com/meisnate12/PMM-TOP10/master/top10.yml"
+base_url = "https://raw.githubusercontent.com/meisnate12/PMM-TOP10/master/"
 builders = ["flixpatrol_top"]
 
 
 class FlixPatrol:
     def __init__(self, config):
         self.config = config
-        self._data = None
         self._platforms = None
         self._locations = None
+        self.country_data = {}
 
-    @property
-    def data(self):
-        if self._data is None:
-            self._data = self.config.load_yaml(ids_url)
-        return self._data
+    def load_info(self):
+        info = self.config.load_yaml(f"{base_url}info.yml")
+        self._platforms = info["platforms"]
+        self._locations = info["locations"]
+
+    def get_country(self, country):
+        if country not in self.country_data:
+            self.country_data[country] = self.config.load_yaml(f"{base_url}lists/{country}.yml")
+        return self.country_data[country]
 
     @property
     def platforms(self):
         if self._platforms is None:
-            self._platforms = [platform for platform in self.data]
-            self._platforms.sort()
+            self.load_info()
         return self._platforms
 
     @property
     def locations(self):
         if self._locations is None:
-            self._locations = []
-            for platform in self.data:
-                self._locations.extend([loc for loc in self.data[platform] if loc not in self._locations and loc != "world"])
-            self._locations.sort()
-            self._locations = ["world"] + self._locations
+            self.load_info()
         return self._locations
-
-    def validate_builder(self, method, data, is_movie):
-        builder_type = "movies" if is_movie else "shows"
-        if method == "flixpatrol_top" and data["location"] in self.data[data["platform"]] and builder_type in self.data[data["platform"]][data["location"]]:
-            return data
-        raise Failed(f"FlixPatrol Error: No {builder_type[:-1].capitalize()} Data Found for {data['platform']} in {data['location']}")
 
     def get_tmdb_ids(self, method, data, is_movie):
         flix_items = []
+        media_type = "movies" if is_movie else "shows"
         if method == "flixpatrol_top":
-            logger.info("Processing FlixPatrol Top:")
+            logger.info(f"Processing FlixPatrol Top {media_type.capitalize()}:")
             logger.info(f"\tPlatform: {data['platform'].replace('_', ' ').title()}")
             logger.info(f"\tLocation: {data['location'].replace('_', ' ').title()}")
+            logger.info(f"\tIn The Last: {data['in_the_last']}")
             logger.info(f"\tLimit: {data['limit']}")
-            flix_items = self.data[data["platform"]][data["location"]]["movies" if is_movie else "shows"][:data["limit"]]
+            country_info = self.get_country(data["location"])
+            for key in [k for k in country_info][:data["in_the_last"]]:
+                if data["platform"] in country_info[key] and media_type in country_info[key][data["platform"]]:
+                    for item in country_info[key][data["platform"]][media_type]:
+                        if item not in flix_items and len(flix_items) < data["limit"]:
+                            flix_items.append(item)
         items = [(i, "tmdb" if is_movie else "tmdb_show") for i in flix_items]
         if len(items) > 0:
             logger.info(f"Processed {len(items)} TMDb IDs")
             return items
         else:
-            raise Failed(f"FlixPatrol Error: No List Items found in {data}")
+            raise Failed(f"FlixPatrol Error: No {media_type.capitalize()} Found for {data['platform'].replace('_', ' ').title()} in {data['location'].replace('_', ' ').title()}")
