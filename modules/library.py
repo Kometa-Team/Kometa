c8910efc7db0da8d9ab4909614fdcfd9316ab583
meisnate12 <meisnate12@gmail.com>
2023-12-26 17:07:38 -0500

    [62] small fixes

--- modules/library.py
+++ modules/library.py
@@ -208,12 +208,12 @@ class Library(ABC):
                             item.removeLabel("Overlay")
                     self._upload_image(item, poster)
                     poster_uploaded = True
-                    logger.info(f"Detail: {poster.attribute} updated {poster.message}")
+                    logger.info(f"Metadata: {poster.attribute} updated {poster.message}")
                 elif self.show_asset_not_needed:
-                    logger.info(f"Detail: {poster.prefix}poster update not needed")
+                    logger.info(f"Metadata: {poster.prefix}poster update not needed")
             except Failed:
                 logger.stacktrace()
-                logger.error(f"Detail: {poster.attribute} failed to update {poster.message}")
+                logger.error(f"Metadata: {poster.attribute} failed to update {poster.message}")
 
         background_uploaded = False
         if background is not None:
@@ -224,12 +224,12 @@ class Library(ABC):
                 if not image_compare or str(background.compare) != str(image_compare):
                     self._upload_image(item, background)
                     background_uploaded = True
-                    logger.info(f"Detail: {background.attribute} updated {background.message}")
+                    logger.info(f"Metadata: {background.attribute} updated {background.message}")
                 elif self.show_asset_not_needed:
-                    logger.info(f"Detail: {background.prefix}background update not needed")
+                    logger.info(f"Metadata: {background.prefix}background update not needed")
             except Failed:
                 logger.stacktrace()
-                logger.error(f"Detail: {background.attribute} failed to update {background.message}")
+                logger.error(f"Metadata: {background.attribute} failed to update {background.message}")
         if self.config.Cache:
             if poster_uploaded:
                 self.config.Cache.update_image_map(item.ratingKey, self.image_table_name, "", poster.compare if poster else "")
