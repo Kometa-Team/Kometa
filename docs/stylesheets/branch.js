653028d8deb32ff144327bfea9eb88dd29a4c7ea
YozoraXCII <96386153+YozoraXCII@users.noreply.github.com>
2023-12-31 17:38:17 +0000

    [66] update

--- docs/stylesheets/branch.js
+++ docs/stylesheets/branch.js
@@ -21,9 +21,9 @@ function checkURLForBranch() {
   }
 
   // Change theme based on URL segment
-  if (currentURL.includes("nightly")) {
+  if (currentURL.includes("en/nightly")) {
     updateTheme("#262dbd", "#262dbd", "PMM Nightly Wiki");
-  } else if (currentURL.includes("develop")) {
+  } else if (currentURL.includes("en/develop")) {
     updateTheme("#ffa724", "#ffa724", "PMM Develop Wiki");
   }
 }
