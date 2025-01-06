c153120186a3b176b361a655ff060b0b51c5d38a
Yozora <yozoraxcii@gmail.com>
2025-01-06 18:44:53 +0000

    Add Branch Detection Logging

--- kometa.py
+++ kometa.py
@@ -209,8 +209,8 @@ elif not os.path.exists(os.path.join(default_dir, "config.yml")):
             print(f"Config Error: Unable to copy the Template file (config.yml.template) from {template_path} to {config_path}. Details: {e}")
             sys.exit(1)
     else:
-        github_branch = git_branch if git_branch else "master"
-        github_url = f"https://raw.githubusercontent.com/Kometa-Team/Kometa/{github_branch}/config/config.yml.template"
+        git_branch = git_branch or "master"
+        github_url = f"https://raw.githubusercontent.com/Kometa-Team/Kometa/{git_branch}/config/config.yml.template"
         try:
             response = requests.get(github_url, timeout=10)
             if response.status_code == 200:
@@ -221,10 +221,10 @@ elif not os.path.exists(os.path.join(default_dir, "config.yml")):
                 print(f"A Configuration file (config.yml) has been downloaded from GitHub and saved as {config_path}. Please update this file with your API keys and other required settings.")
                 sys.exit(1)
             else:
-                print(f"Config Error: No Configuration file (config.yml) or Template file (config.yml.template) found in the config path, and the template file(config.yml.template) could not be downloaded from GitHub. Please visit the GitHub repository to manually download the config.yml.template file and place it in {template_path} prior to running Kometa again.")
+                print(f"Config Error: No Configuration file (config.yml) or Template file (config.yml.template) found in the config path, and the template file(config.yml.template) could not be downloaded from GitHub (Branch {git_branch}). Please visit the GitHub repository to manually download the config.yml.template file and place it in {template_path} prior to running Kometa again.")
                 sys.exit(1)
         except requests.RequestException as e:
-            print(f"Config Error: Failed to download the configuration template file (config.yml.template) from GitHub. Details: {e}")
+            print(f"Config Error: Failed to download the configuration template file (config.yml.template) from GitHub. Details: {e}. Branch Detected: {git_branch}")
             sys.exit(1)
 
 
