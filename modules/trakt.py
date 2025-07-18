73502422ab7209c22149acbc0c05172dd235e4b4
Chaz Larson <chaz@chazlarson.com>
2025-07-18 13:50:47 -0500

    report actual response code

--- modules/trakt.py
+++ modules/trakt.py
@@ -226,6 +226,7 @@ class Trakt:
             if pages == 1 and "X-Pagination-Page-Count" in response.headers and not params:
                 pages = int(response.headers["X-Pagination-Page-Count"])
             if response.status_code >= 400:
+                logger.debug(f"Trakt response issue: ({response.status_code}) {response.reason}")
                 raise Failed(f"({response.status_code}) {response.reason}")
             response_json = response.json()
             logger.trace(f"Headers: {response.headers}")
