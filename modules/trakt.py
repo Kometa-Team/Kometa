1e691326604fec2fdb4a875e4e04c3dc4ef76dde
Chaz Larson <chaz@chazlarson.com>
2025-11-06 13:30:09 -0600

    [1] Fix Trakt Reauth Loop on Private Lists (#2845)

--- modules/trakt.py
+++ modules/trakt.py
@@ -206,6 +206,8 @@ class Trakt:
             params = {}
         pages = 1
         current = 1
+        reauth_count = 0
+        auth_delay = 3
         logger.trace(f"URL: {base_url}{url}")
         if params:
             logger.trace(f"Params: {params}")
@@ -227,13 +229,20 @@ class Trakt:
             if pages == 1 and "X-Pagination-Page-Count" in response.headers and not params:
                 pages = int(response.headers["X-Pagination-Page-Count"])
             if response.status_code == 401:
+                time.sleep(auth_delay)
+                auth_delay += 3
                 if not self._refresh():
                     logger.debug(f"Trakt token refresh failure")
+                    raise Failed(f"({response.status_code}) {response.reason}")
+                reauth_count += 1
+                if reauth_count > 1:
+                    logger.debug(f"Trakt token has been refreshed twice on this request; this may be a private list")
                     raise Failed(f"({response.status_code}) {response.reason}")
             elif response.status_code >= 400:
                 logger.debug(f"Trakt response issue: ({response.status_code}) {response.reason}")
                 raise Failed(f"({response.status_code}) {response.reason}")
             else:
+                reauth_count = 0
                 response_json = response.json()
                 logger.trace(f"Headers: {response.headers}")
                 logger.trace(f"Response: {response_json}")
